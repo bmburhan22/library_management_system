01b5503b020a542b55b552a23b64354925130801
Burhanuddin <bm.burhan22@gmail.com>
2024-09-27 02:06:16 +0530

    Add testcases for returnBook

--- __tests__/Library.test.ts
+++ __tests__/Library.test.ts
@@ -1,4 +1,4 @@
-import Library, {BookAlreadyBorrowedError, BookNotFoundError} from '../Library';
+import Library, { BookAlreadyBorrowedError, BookNotFoundError } from '../Library';
 
 describe('Library Management System', () => {
   let library: Library;
@@ -36,6 +36,26 @@ describe('Library Management System', () => {
     library.addBook(5343, 'Python 101', 'Jane White', 2018);
     library.borrowBook(5343);
     expect(() => library.borrowBook(5343)).toThrowError(new BookAlreadyBorrowedError(5343));
+  });
+
+  // Testcases for returnBook
+  test('Allow returning a borrowed book', () => {
+    library.addBook(5343, 'Python 101', 'Jane White', 2018);
+    library.borrowBook(5343);
+    library.returnBook(5343);
+    const availableBooks = library.viewAvailableBooks();
+    expect(availableBooks.length).toBe(1);
+    expect(availableBooks[0]).toEqual({
+      isbn: 5343,
+      title: 'Python 101',
+      author: 'Jane White',
+      publicationYear: 2018,
+      isBorrowed: false,
+    });
+  });
+
+  test('Throw error when returning a book that does not exist', () => {
+    expect(() => library.returnBook(777)).toThrowError(new BookNotFoundError(777));
   });
 
 });
