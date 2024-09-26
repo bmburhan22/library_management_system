40f7ebf55898439c729563c8fa2eea2da6eb6c79
Burhanuddin <bm.burhan22@gmail.com>
2024-09-27 02:07:49 +0530

    Add testcase for viewAvailableBook and pass the testcase

--- __tests__/Library.test.ts
+++ __tests__/Library.test.ts
@@ -58,4 +58,19 @@ describe('Library Management System', () => {
     expect(() => library.returnBook(777)).toThrowError(new BookNotFoundError(777));
   });
 
+  test('View available books', () => {
+    library.addBook(5343, 'Python 101', 'Jane White', 2018);
+    library.addBook(5344, 'Learn Flutter Basics', 'Bob Mark', 2020);
+    library.borrowBook(5343);
+    const availableBooks = library.viewAvailableBooks();
+    expect(availableBooks.length).toBe(1);
+    expect(availableBooks[0]).toEqual({
+      isbn: 5344,
+      title: 'Learn Flutter Basics',
+      author: 'Bob Mark',
+      publicationYear: 2020,
+      isBorrowed: false
+    });
+  });
+
 });
